0037a693e01149c3ff600ed7d3c291c36867a648
Javier Ventura <jventura1097@gmail.com>
2019-02-25 21:07:02 -0800

    updated Navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,7 +5,7 @@ function Navbar(props) {
   return (
 
     <nav class="navbar navbar-dark bg-dark fixed-top">
-      <a class="navbar-brand" href="https://jventura10.github.io/portfolio/">Navbar</a>
+      <a class="navbar-brand" href="https://jventura10.github.io/portfolio/">Javier Ventura</a>
       <span class="navbar-text">Score: {props.count}</span>
     </nav>
   );
