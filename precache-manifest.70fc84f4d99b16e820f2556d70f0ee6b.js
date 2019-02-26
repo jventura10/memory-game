04fd20a5cdecab774c24f2ddd439d553d518e8af
Javier Ventura <jventura1097@gmail.com>
2019-02-25 21:00:13 -0800

    Updates

---
+++ precache-manifest.70fc84f4d99b16e820f2556d70f0ee6b.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "554fd01f6744f978583a68f2e10bfe2b",
+    "url": "/memory-game/static/media/Might Guy.554fd01f.png"
+  },
+  {
+    "revision": "2589a53e433a506610b4",
+    "url": "/memory-game/static/css/main.654428a4.chunk.css"
+  },
+  {
+    "revision": "435a81855f49d55acd25",
+    "url": "/memory-game/static/js/runtime~main.435a8185.js"
+  },
+  {
+    "revision": "aa7fbe6322facae59b89",
+    "url": "/memory-game/static/js/2.aa7fbe63.chunk.js"
+  },
+  {
+    "revision": "fab0514cdbc8e58ca588ae3e0418c23e",
+    "url": "/memory-game/static/media/Deidara.fab0514c.png"
+  },
+  {
+    "revision": "807b10287d7177d8b0171e8134f80c96",
+    "url": "/memory-game/static/media/Haku.807b1028.png"
+  },
+  {
+    "revision": "211184fea3ab510c0bd91f92584d0305",
+    "url": "/memory-game/static/media/Hashirama.211184fe.png"
+  },
+  {
+    "revision": "1e0234d4eaaf6421b31007cc6473d67f",
+    "url": "/memory-game/static/media/Hiruzen.1e0234d4.png"
+  },
+  {
+    "revision": "3cb0b55699dfad64b69ae1801a1f721b",
+    "url": "/memory-game/static/media/Itachi.3cb0b556.png"
+  },
+  {
+    "revision": "c2b686a088bc60db6e60c926a2eaa377",
+    "url": "/memory-game/static/media/Kakashi.c2b686a0.png"
+  },
+  {
+    "revision": "b275faf242f4296d564f10a4e6be39eb",
+    "url": "/memory-game/static/media/Kisame.b275faf2.png"
+  },
+  {
+    "revision": "1db3ab1e904aa4a8343d10a4198acdb3",
+    "url": "/memory-game/static/media/Madara.1db3ab1e.png"
+  },
+  {
+    "revision": "2589a53e433a506610b4",
+    "url": "/memory-game/static/js/main.2589a53e.chunk.js"
+  },
+  {
+    "revision": "4df4928870c974c04a905868a3521db7",
+    "url": "/memory-game/static/media/Minato.4df49288.png"
+  },
+  {
+    "revision": "849d471ea451e953782fedbc30b790ce",
+    "url": "/memory-game/static/media/Naruto.849d471e.png"
+  },
+  {
+    "revision": "fa22b409ba1ad12f897bfab514f25672",
+    "url": "/memory-game/static/media/Obito.fa22b409.png"
+  },
+  {
+    "revision": "431e84772ab72ad9b84d8a539f0c77a7",
+    "url": "/memory-game/static/media/Orochimaru.431e8477.png"
+  },
+  {
+    "revision": "e45020c5919ff8b2c5dc4ccd3230e8e3",
+    "url": "/memory-game/static/media/Sasuke.e45020c5.png"
+  },
+  {
+    "revision": "726ad4bca527ffd77660a3a2af533e4b",
+    "url": "/memory-game/static/media/Shikamaru.726ad4bc.png"
+  },
+  {
+    "revision": "3ac69f3f7c981f08b47ea2f9a2dc7b4f",
+    "url": "/memory-game/static/media/Shisui.3ac69f3f.png"
+  },
+  {
+    "revision": "ad9d804dbeba7ed0316554ea851475cb",
+    "url": "/memory-game/static/media/Tobirama.ad9d804d.png"
+  },
+  {
+    "revision": "6f9ce5043627ab34244fa4a4347c31a5",
+    "url": "/memory-game/static/media/Zabuza.6f9ce504.png"
+  },
+  {
+    "revision": "a76268006595fbb56a0dc04c81e9b6f7",
+    "url": "/memory-game/static/media/bg.a7626800.png"
+  },
+  {
+    "revision": "b0c368d8c022591ded6d3db7f7b040d1",
+    "url": "/memory-game/index.html"
+  }
+];
